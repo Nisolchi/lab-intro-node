61b14cfaa6a5d0241fac6e11cfb3c356b194d4ca
nisol <nisolmedinaue@gmail.com>
2024-04-29 17:06:43 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items =[];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a,b)=> a-b);
+  }
 
-  get(pos) {}
+  
 
-  max() {}
+  get(pos) {
+    if(pos < 0 || pos >= this.items.length){
+      throw new Error("OutOfBounds");
+    }
+       return this.items[pos];
+       
+      }
 
-  min() {}
+      
+  max() {
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
 
-  sum() {}
+    }
+     return this.items[this.items.length - 1]
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+
+  }
+  return this.items[0]
 }
+
+  sum() {
+    if (this.items.length === 0) {
+      return 0;
+  }
+  return this.items.reduce((acc, current) => acc + current, 0);
+}
+
+
+
+
+  avg() {
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+ return this.items.reduce((acc,current) => acc + current, 0) / this.items.length;
+}
+
+  }
+
+
 
 module.exports = SortedList;
